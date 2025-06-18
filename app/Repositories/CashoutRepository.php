8cae08ac4b902fad0695f173759930d82f4bdba6
hesen501 <hesenhesenov177@gmail.com>
2025-06-18 11:07:44 +0400

    fix

--- app/Repositories/CashoutRepository.php
+++ app/Repositories/CashoutRepository.php
@@ -21,22 +21,4 @@ class CashoutRepository
             ->whereBetween('created_at', [time(), time() + 86400])
             ->sum('amount');
     }
-
-    public function create($data)
-    {
-        return User::query()->create($data);
-    }
-
-    public function update($id, $data)
-    {
-        $user = User::query()->findOrFail($id);
-        $user->update($data);
-        return $user;
-    }
-
-    public function delete($id)
-    {
-        $user = User::query()->findOrFail($id);
-        return $user->delete();
-    }
 }
